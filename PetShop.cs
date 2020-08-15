f4df0407cb7456f3e809be0b8fbf95717aae9cd5
Erick Douglas <erick-douglas@hotmail.com>
2020-08-15 15:53:55 -0300

    first commit

---
+++ PetShop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+namespace Projetos_C_
+{
+    class PetShop
+    {
+        private double meucaninofeliz;
+        private double vairex;
+        private double chowchawgas;
+        public void BanhoFinalSemana(int cachorroP, int cachorroG){            
+            meucaninofeliz=((cachorroP*(20*1.2))+cachorroG*(40*1.2));
+            vairex=((cachorroP*20)+(cachorroG*55));
+            chowchawgas=((cachorroP*30)+(cachorroG*45));
+            if (chowchawgas<=vairex && chowchawgas<=meucaninofeliz){
+                Console.WriteLine("\nO Melhor Petshop para o dia escolhido foi Chow Chawgas: "+"R$"+chowchawgas);
+                Console.ReadLine();
+            } else if(vairex<chowchawgas && vairex<=meucaninofeliz){
+                Console.WriteLine("\nO Melhor Petshop para o dia escolhido foi Vai Rex: "+"R$"+vairex);
+                Console.ReadLine();
+            } else{
+                Console.WriteLine("\nO Melhor Petshop para o dia escolhido foi Meu Canino Feliz: "+"R$"+meucaninofeliz);
+                Console.ReadLine();
+            }            
+        }
+        public void BanhoSemana(int cachorroP, int cachorroG){
+            meucaninofeliz=((cachorroP*20)+(cachorroG*40));
+            vairex=((cachorroP*15)+(cachorroG*50));
+            chowchawgas=((cachorroP*30)+(cachorroG*45));
+            if (chowchawgas<=vairex && chowchawgas<=meucaninofeliz){
+                Console.WriteLine("\nO Melhor Petshop para o dia escolhido foi Chow Chawgas: "+"R$"+chowchawgas);
+                Console.ReadLine();
+            } else if(vairex<chowchawgas && vairex<=meucaninofeliz){
+                Console.WriteLine("\nO Melhor Petshop para o dia escolhido foi Vai Rex: "+"R$"+vairex);
+                Console.ReadLine();
+            } else{
+                Console.WriteLine("\nO Melhor Petshop para o dia escolhido foi Meu Canino Feliz: "+"R$"+meucaninofeliz);
+                Console.ReadLine();
+            }
+        }        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+namespace Projetos_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cachorroP,cachorroG,numerodia;
+            PetShop pet = new PetShop();
+                        
+            Console.Write("Digite a quantidade de Cachorros PEQUENOS para o Banho: ");
+            cachorroP=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite a quantidade de Cachorros GRANDES para o Banho: ");
+            cachorroG=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite a Data para o Banho ");
+            Console.WriteLine("Exemplo: 03/08/2018\n");
+            DateTime diasemana = DateTime.Parse(Console.ReadLine());
+            numerodia=((int) diasemana.DayOfWeek);
+            if(numerodia==6 || numerodia==0){
+                pet.BanhoFinalSemana(cachorroP,cachorroG);
+            }else
+                pet.BanhoSemana(cachorroP,cachorroG);
+
+        }
+    }
+}
